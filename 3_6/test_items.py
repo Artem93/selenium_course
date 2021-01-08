fadd5bc0bbcb7931e798791ac1003bb01756a078
Artem93 <artegor1993@gmail.com>
2021-01-08 20:48:26 +0300

    Update test_items.py

--- 3_6/test_items.py
+++ 3_6/test_items.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 import time
 
 
-def test_items(browser: webdriver.Chrome):
+def test_item_has_add_to_cart_button(browser: webdriver.Chrome):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
     count_basket_btn = browser.find_elements_by_class_name("btn-add-to-basket").__len__()
